9af5d6a08a7e50b1fa9006cc2de8479719d0daac
jeremienguyen20798 <phuchoang20798@gmail.com>
2026-03-16 15:46:08 +0700

    Change api key for Gemini API

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,6 +1,7 @@
-const apiKey = 'AIzaSyDA42y3osEDcBNFrMu9Ud1KbbFHYom4Qek';
+const apiKey = 'AIzaSyCFwaYo84aaofGS7m-2zGGzAmjxxFZr2Cs';
 const messageTemplateKey = 'message_template';
 const messageTitle = 'Mẫu tin nhắn';
 const messageHint = 'Nhập nội dung tin nhắn mẫu...';
 const textPrompt =
     'Đọc và phân tích bức ảnh bên dưới, cho tôi biết: Phương thức thanh toán là gì, số tiền bao nhiêu. Trả lời ngắn gọn, chỉ cần thông tin cần thiết, không cần giải thích thêm.';
+const appTitle = 'Lịch sử cuộc gọi';
