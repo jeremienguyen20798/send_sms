ff132e97b06532df426cf3785ef7ba52e5fe3831
jeremienguyen20798 <phuchoang20798@gmail.com>
2026-03-23 22:17:08 +0700

    Add request default SMS App to send MMS

---
+++ lib/featres/settings/bloc/settings_state.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+abstract class SettingsState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class InitialSettingsState extends SettingsState {}
